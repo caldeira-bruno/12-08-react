c5939c440c58870c47a42ba334e1ee8f72d69f22
Bruno <0064121@academico.ifmg.edu.br>
2024-08-12 14:30:02 -0300

    atualizações

--- src/app.mjs
+++ src/app.mjs
@@ -1,12 +1,25 @@
 import List from "./components/list.mjs";
+import Table from "./components/table.mjs"
 
 const cores = ['Azul', 'Branco', 'Verde'];
+const titles = ['Id', 'Nome', 'Idade']
+const clients = [
+    {id: 1, nome: "Lucas", idade: 22},
+    {id: 2, nome: "Ruan", idade: 28},
+    {id: 3, nome: "Matt", idade: 24},
+    {id: 4, nome: "Raul", idade: 23},
+];
 
 const App = () => {
     return (
         <div>
-            <h2>Hello</h2>;
+            <h2>Hello</h2>
+            <h3>Cores:</h3>
             <List data={cores}/>
+            <h3>Felinos</h3>
+            <List data={['Onça', 'Gato', 'Leao']}/>
+            <h3>Tabela</h3>
+            <Table titles={titles} data={clients}/>
         </div>
     );
 };

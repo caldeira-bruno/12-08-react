fc53816651516517c61399929dd6f043968cb11a
Bruno <0064121@academico.ifmg.edu.br>
2024-08-13 15:57:15 -0300

    componentes select

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,7 @@
 import List from "./components/list.mjs";
 import Table from "./components/table.mjs"
+import { useRef, useState } from 'react';
+import Select from "./components/select.mjs";
 
 const cores = ['Azul', 'Branco', 'Verde'];
 const titles = ['Id', 'Nome', 'Idade']
@@ -11,15 +13,49 @@ const clients = [
 ];
 
 const App = () => {
+    const [nome, setNome] = useState('');
+    const [felinos, setFelinos] = useState(['Onça', 'Gato', 'Leao']);
+
+    const iptFelino = useRef(null);
+
+    function clique(e) {
+        setNome("Novo nome");
+    }
+    function change(e) {
+        setNome(e.target.value);
+    }
+    function manipulaFormFelinos(e) {
+        e.preventDefault();
+        setFelinos([...felinos, iptFelino.current.value]);
+    }
     return (
         <div>
-            <h2>Hello</h2>
+            <div>
+            <h2>Hello {nome}</h2>
+            <input onChange={change}/>
+            <button onClick={clique}>Cliques</button>
+            <div>
             <h3>Cores:</h3>
             <List data={cores}/>
+            </div>
+            </div>
+            <div>
             <h3>Felinos</h3>
-            <List data={['Onça', 'Gato', 'Leao']}/>
+            <form onSubmit={manipulaFormFelinos}>
+                <label>Novo</label>
+                <input ref={iptFelino}/>
+                <button>+</button>
+            </form>
+            <List data={felinos}/>
+            </div>
+            <div>
             <h3>Tabela</h3>
             <Table titles={titles} data={clients}/>
+            </div>
+            <div>
+                <h3>Municipios</h3>
+                <Select/>
+            </div>
         </div>
     );
 };
